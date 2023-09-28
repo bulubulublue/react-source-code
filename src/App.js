0ee1296b31b9288be599126f2740e477b6a7a5b7
shen qin <shenqin@marketnode.com>
2023-09-28 14:37:38 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import Route from "./components/Route.tsx";
-import Link from "./components/Link.tsx";
-import { Home, About } from "./pages/index";
-import Topics from "./pages/Topics";
+
 
 export default function App() {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
 
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
     </div>
   );
 }
